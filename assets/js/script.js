61a98666d99b88804b4b1b760daf31dda778e752
mibrahim234 <mohamm1417@gmail.com>
2021-09-24 19:43:27 -0400

    fixed up footer so it does not block content

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,16 +103,16 @@ const getRestaurantInfo = function(zipcode) {
     })
 }
 
-    console.log(fetch('https://api.giphy.com/v1/gifs/search?q=cheeseburgers&api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN'))
-    then(res => {
-        if(res.ok) {
-            console.log('SUCCESS')
-        }   else {
-            console.log("UNSUCCESSFUL")
-        }
-    })
-    .then(res => console.log(data))
-    .catch(error => console.log('No Cheeseburger For You'))
+    // console.log(fetch('https://api.giphy.com/v1/gifs/search?q=cheeseburgers&api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN'))
+    // .then(res => {
+    //     if(res.ok) {
+    //         console.log('SUCCESS')
+    //     }   else {
+    //         console.log("UNSUCCESSFUL")
+    //     }
+    // })
+    // .then(res => console.log(data))
+    // .catch(error => console.log('No Cheeseburger For You'))
 
 const formSubmitHandler = function(event) {
     event.preventDefault();
